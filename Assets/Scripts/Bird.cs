4d156131f90b4ca715df02620be1e349bd15cc39
Maren Veka <mveka6979@student.pps.net>
2024-03-13 10:00:41 -0700

    commit 2

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -1,16 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.Security.Cryptography;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Bird : MonoBehaviour
 {
+    [SerializeField] float maxDragDistance = 2;
     [SerializeField] float launchPower = 250;
+    LineRenderer lineRenderer;
     Vector3 startingPosition;
 
     void Start()
     {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.SetPosition(0, transform.position);
+        lineRenderer.enabled = false;
         startingPosition = transform.position;
     }
 
@@ -19,13 +21,18 @@ public class Bird : MonoBehaviour
         Vector3 directionAndMagnitude = startingPosition - transform.position;
         GetComponent<Rigidbody2D>().AddForce(directionAndMagnitude * launchPower);
         GetComponent<Rigidbody2D>().gravityScale = 1;
+        lineRenderer.enabled = false;
     }
 
     void OnMouseDrag()
     {
         Vector3 destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         destination.z = 0;
+        if (Vector2.Distance(destination, startingPosition) > maxDragDistance)
+            destination = Vector3.MoveTowards(startingPosition, destination, maxDragDistance);
         transform.position = destination;
+        lineRenderer.SetPosition(1, transform.position);
+        lineRenderer.enabled = true;
     }
 
     // Update is called once per frame
@@ -33,5 +40,12 @@ public class Bird : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Q))
             GetComponent<Rigidbody2D>().gravityScale = 1;
+
+        if (FindAnyObjectByType<Enemy>(FindObjectsInactive.Exclude) == null)
+        {
+            Debug.Log("Game Over");
+            var levelToLoad = SceneManager.GetActiveScene().buildIndex + 1;
+            SceneManager.LoadScene(levelToLoad);
+        }
     }
 }
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+
+    [SerializeField] float destroyImpactMagnitude = 5;
+
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.GetComponent<Bird>())
+            Destroy(gameObject);
+
+        if (collision.relativeVelocity.magnitude > destroyImpactMagnitude)
             Destroy(gameObject);
     }
 }
